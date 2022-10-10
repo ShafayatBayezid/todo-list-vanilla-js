2c961d2c51f762b9f21939ccfb9fac7981ec2cd8
Shafayat Hossain Bayezid <54271386+ShafayatBayezid@users.noreply.github.com>
2022-10-10 18:02:50 +0600

    something

---
+++ README.md
@@ -0,0 +1,3 @@
+### `Live Demo`
+
+Visit [https://shafayat.net/todoAppVanillaJS](https://shafayat.net/todoAppVanillaJS/) to view it in your browser.
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,17 @@ window.addEventListener("load", (e) => {
   var textInput = document.querySelector("#note");
   var listArea = document.querySelector(".list-area");
 
+  //get form element from DOM
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     var notes = textInput.value;
 
+    //check if the input element is empty
     if (!notes) {
       alert("Please add your note first");
     }
 
+    //create elements for note list
     var list_el = document.createElement("div");
     list_el.classList.add("list");
     var p_el = document.createElement("p");
@@ -23,5 +26,18 @@ window.addEventListener("load", (e) => {
     list_el.appendChild(p_el);
     list_el.appendChild(span_el);
     listArea.appendChild(list_el);
+  });
+
+    
+    var allNotes = document.querySelector(".list-area");
+console.log(allNotes.lastChild);
+    
+  //delete unnessecery list from DOM
+  var note = document.querySelector('span');
+  note.addEventListener("click", (e) => {
+    // var allNotes = document.querySelector(".list-area");
+    // allNotes.removeChild(allNotes.firstElementChild);
+      
+      alert('dfadsf')
   });
 });

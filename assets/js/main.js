4ecb308b26b5f0dd134ebdd852e53ab43668035a
Shafayat Hossain Bayezid <54271386+ShafayatBayezid@users.noreply.github.com>
2022-10-11 12:19:28 +0600

    todo complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,33 +11,27 @@ window.addEventListener("load", (e) => {
     //check if the input element is empty
     if (!notes) {
       alert("Please add your note first");
+    } else {
+      //create elements for note list
+      var list_el = document.createElement("div");
+      list_el.classList.add("list");
+      var p_el = document.createElement("p");
+      var span_el = document.createElement("span");
+      span_el.classList.add("material-icons-outlined");
+      span_el.innerText = "clear";
+
+      p_el.innerText = notes;
+
+      list_el.appendChild(p_el);
+      list_el.appendChild(span_el);
+      listArea.appendChild(list_el);
+
+      textInput.value = "";
+
+      // delete unnessecery list from DOM
+      span_el.addEventListener("click", () => {
+        listArea.removeChild(list_el);
+      });
     }
-
-    //create elements for note list
-    var list_el = document.createElement("div");
-    list_el.classList.add("list");
-    var p_el = document.createElement("p");
-    var span_el = document.createElement("span");
-    span_el.classList.add("material-icons-outlined");
-    span_el.innerText = "clear";
-
-    p_el.innerText = notes;
-
-    list_el.appendChild(p_el);
-    list_el.appendChild(span_el);
-    listArea.appendChild(list_el);
-  });
-
-    
-    var allNotes = document.querySelector(".list-area");
-console.log(allNotes.lastChild);
-    
-  //delete unnessecery list from DOM
-  var note = document.querySelector('span');
-  note.addEventListener("click", (e) => {
-    // var allNotes = document.querySelector(".list-area");
-    // allNotes.removeChild(allNotes.firstElementChild);
-      
-      alert('dfadsf')
   });
 });
